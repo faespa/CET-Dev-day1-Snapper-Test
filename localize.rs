e8e99f1e694ed9f5b2e0315451c27dce949b27d6
Faespa <35931428+faespa@users.noreply.github.com>
2022-06-17 00:33:54 -0500

    streth and Connectors
    build models and and streth and connectors

---
+++ localize.rs
@@ -0,0 +1,59 @@
+﻿package profile.fparra.extensionfp;
+
+/**
+ * In this file you put localized strings for all languages you want to support.
+ * To use the resource string in your code, you just type its name like a constant, like this:
+ * 
+ * pln($sampleItems);
+ * 
+ */
+
+
+$sampleItems {
+    english "Sample Items";
+    swedish "Testföremål";
+}
+
+$basicBoxPart {
+    english "A basic box part";
+    swedish "En enkel låd-artikel";
+}
+
+/**
+ * CET Designer will automatically look for a snapper tooltip string with the name:
+ * $ClassNameTip where 'ClassName' is the name of your class.
+ * So the tooltip for 'BasicBox' (in file basicBox.cm) is $BasicBoxTip
+ * (you can also return a custom tooltip text by overriding method 'toolTipText()' in your class).
+ */
+$BasicBoxTip {
+    english "A basic box";
+    swedish "En enkel låda";
+}
+
+$width {
+    english "Width";
+}
+
+$height {
+    english "Height";
+}
+$doorS.right {
+    english "Right";
+}
+$doorS.left {
+    english "Left";
+}
+
+$mat.one {
+    english "Mahogny";
+}
+$mat.two {
+    english "Maple";
+}
+$mat.three {
+    english "Cherry";
+}
+
+$Cabinet1 {
+    english "Kitchen Cabinet";
+}
